9eab21bf7bb622376b2446e41947a23002d6c0d4
Arkoz / Ludo <93744170+Arkozzz@users.noreply.github.com>
2023-12-14 16:20:00 +0100

    Update Partie_4.py

--- Partie_4.py
+++ Partie_4.py
@@ -1,10 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
+from scipy.io.wavfile import write
 import soundfile as sf
 import os
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 audio_folder = os.path.dirname(os.path.abspath(__file__))+"\son"
+
+def generate_audio(fundamental_freq, harmonics, fundamental_amp, harmonics_amp, duration, sample_rate):
+    # Créez un tableau de temps pour la durée spécifiée
+    print(len(harmonics))
+    t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+
+    # Initialisez le signal audio avec des zéros
+    audio_data = np.zeros(len(t))
+
+    # Ajoutez la composante fondamentale
+    audio_data += fundamental_amp * np.sin(2 * np.pi * fundamental_freq * t)
+
+    # Ajoutez les harmoniques
+    for i, harmonic_freq in enumerate(harmonics):
+        audio_data += harmonics_amp[i] * np.sin(2 * np.pi * harmonic_freq * t)
+
+    # Normalisez le signal audio (facultatif)
+    audio_data /= np.max(np.abs(audio_data))
+
+    # Enregistrez le signal audio dans un fichier WAV
+    write(f"artificial_sound/audio_{note}.wav", sample_rate, audio_data.astype(np.float32))
 
 audio_files = {
     'Note_01': 'Note_01.aiff',
@@ -64,11 +87,17 @@ for note, file_name in audio_files.items():
     plt.xlabel("Frequency (Hz)")
     plt.ylabel("Amplitude")
     plt.xlim(0, 2000)
-    plt.show()
+    #plt.show()
 
     # Fréquence fondamentale et harmoniques
     fundamental_freq = xf[peaks][0]
-    harmonics = xf[peaks][1:]
+    # Trier les fréquences des harmoniques par proximité avec la fréquence fondamentale
+    harmonics_with_differences = sorted([(freq, abs(freq - fundamental_freq)) for freq in xf[peaks]], key=lambda x: x[1])
+
+    # Sélectionner les 100 harmoniques les plus proches
+    closest_harmonics = harmonics_with_differences[1:10001]  # On exclut la fondamentale
+
+    harmonics = [h[0] for h in closest_harmonics]
     fundamental_amp = (2.0/n * np.abs(yf[:n//2]))[peaks][0]
     harmonics_amp = (2.0/n * np.abs(yf[:n//2]))[peaks][1:]
 
@@ -76,4 +105,5 @@ for note, file_name in audio_files.items():
     print("Fundamental Frequency:", fundamental_freq, "Hz")
     print("Fundamental Amplitude:", fundamental_amp)
     #print("Harmonics:", list(zip(harmonics, harmonics_amp)))
-
+    
+    generate_audio(fundamental_freq, harmonics, fundamental_amp, harmonics_amp, 1, 44100)
